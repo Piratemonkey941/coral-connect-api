003a08d691c3a718efa31821033cf009499f0e20
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2022-11-20 14:03:24 -0600

    worked on initial impiment of fullpage.js

--- moonshiners-app/karma.conf.js
+++ moonshiners-app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/reef-moonshiner-app'),
+      dir: require('path').join(__dirname, './coverage/moonshiners-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- moonshiners-app/src/app/app.component.spec.ts
+++ moonshiners-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'reef-moonshiner-app'`, () => {
+  it(`should have as title 'moonshiners-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('reef-moonshiner-app');
+    expect(app.title).toEqual('moonshiners-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('reef-moonshiner-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('moonshiners-app app is running!');
   });
 });
