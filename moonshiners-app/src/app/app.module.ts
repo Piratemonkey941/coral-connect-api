78bb4c8fd9961e3b7c1a46ccf7a5b089062dface
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2022-11-21 08:20:31 -0600

    Dots Color Changed, added background

--- moonshiners-app/src/app/app.module.ts
+++ moonshiners-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AngularFullpageModule } from '@fullpage/angular-fullpage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavbarComponent} from './navbar/navbar.component'
-import { ShinerSystemComponent } from './shiner-system/shiner-system.component';
+import { ShinerSystemComponent } from './main/shiner-system/shiner-system.component';
 import { BigFourComponent } from './main/big-four/big-four.component';
 import { LandingComponent } from './main/landing/landing.component';
 import { MajorElemsComponent } from './main/major-elems/major-elems.component';
