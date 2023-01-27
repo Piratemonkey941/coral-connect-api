0d030dd9e5e810a69ab9ef58b58d35415ddc3488
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-01-27 11:27:52 -0600

    work on minor elemes
    Added third page for elems and changed UI so there are only 5-6 cards per page for readability.
    Assigned all element names to appropriate cards in html

--- moonshiners-app/src/app/app.module.ts
+++ moonshiners-app/src/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFullpageModule } from '@fullpage/angular-fullpage';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavbarComponent} from './navbar/navbar.component'
-import { ShinerSystemComponent } from './main/shiner-system/shiner-system.component';
 import { BigFourComponent } from './main/big-four/big-four.component';
 import { LandingComponent } from './main/landing/landing.component';
 import { MajorElemsComponent } from './main/major-elems/major-elems.component';
 import { MinorElemsComponent } from './main/minor-elems/minor-elems.component';
 import { MinorElemsTwoComponent } from './main/minor-elems-two/minor-elems-two.component';
+import { MinorElemsThreeComponent } from './main/minor-elems-three/minor-elems-two.component';
+import { ShinerSystemComponent } from './main/shiner-system/shiner-system.component';
 import { ModalComponent } from './shared/modal/modal.component';
 // import { AlkilinityComponent } from './main/big-four/alkilinity/alkilinity.component';
 // import {} from ''
@@ -20,13 +20,14 @@ import { ModalComponent } from './shared/modal/modal.component';
   declarations: [
     AppComponent,
     NavbarComponent,
-    ShinerSystemComponent,
     BigFourComponent,
     LandingComponent,
     MajorElemsComponent ,
+    ModalComponent,
     MinorElemsComponent,
     MinorElemsTwoComponent,
-    ModalComponent,
+    MinorElemsThreeComponent,
+    ShinerSystemComponent,
     // AlkilinityComponent,
 
 
