7ebf3e436dfc18765f1ecc405450fa1e8a6c635f
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-03-15 11:45:59 -0500

    zinc implemented + a refactor by gpt4

--- moonshiners-app/src/app/main/minor-elems-three/minor-elems-three.component.ts
+++ moonshiners-app/src/app/main/minor-elems-three/minor-elems-three.component.ts
@@ -193,13 +193,13 @@ export class MinorElemsThreeComponent implements OnInit {
   onAddTin(){
 
   // general boron calculation
-  this.tinDays = Math.ceil(410 - this.tin)   // 2
-  this.tinQuantityDivisor = (410 - this.tin) // 410 - 4.5 = 1.5
-  this.tinAdjustmentTotal = (0.0701 * this.volumeService.volume) * this.tinQuantityDivisor
+  this.tinDays = Math.ceil(5 - this.tin)   // 2
+  this.tinQuantityDivisor = (5 - this.tin) // 410 - 4.5 = 1.5
+  this.tinAdjustmentTotal = (0.003785 * this.volumeService.volume) * this.tinQuantityDivisor
   this.tinAdjustment = this.tinAdjustmentTotal / this.tinDays
 
 
-    if (this.tin == 10){
+    if (this.tin == 5){
         this.tinStart = 'Ideal  for most reefs'
     }
   //low start  7.72ml at 100 g for 0.1 ppm increase
@@ -213,7 +213,7 @@ export class MinorElemsThreeComponent implements OnInit {
     else if ( this.tin <= 9 && this.tin >= 6.1 ){
       this.tinStart = `Reduced tin, adjust  ${this.tinAdjustment}ml per day for ${this.tinDays} days.  `
     }
-    else if ( this.tin <= 9.9 && this.tin >= 9.1 ){
+    else if ( this.tin <= 4.9 && this.tin >= .1 ){
       this.tinStart = `Optimal Range for tin, adjust ${this.tinAdjustment}ml per day for ${this.tinDays} days.`
       }
 
@@ -233,54 +233,161 @@ export class MinorElemsThreeComponent implements OnInit {
   }
 
           // ==================================================== Zinc ====================================================
-  zincStart: string = 'Instead of becoming fireworks, Im going to make your corals glow!'
-  zinc: number
-  zincAdjustment: any
-  zincAdjustmentTotal: number
-  zincDays: number
-  zincQuantityDivisor: number
 
-  onAddZinc(){
+readonly LOW_ZINC_LEVEL = 1;
+readonly MODERATE_ZINC_LEVEL = 3;
+readonly IDEAL_ZINC_LEVEL = 5;
 
-  // general boron calculation
-  this.zincDays = Math.ceil(410 - this.zinc)   // 2
-  this.zincQuantityDivisor = (410 - this.zinc) // 410 - 4.5 = 1.5
-  this.zincAdjustmentTotal = (0.0701 * this.volumeService.volume) * this.zincQuantityDivisor
-  this.zincAdjustment = this.zincAdjustmentTotal / this.zincDays
+zincStart: string = 'Instead of becoming fireworks, Im going to make your corals glow!';
+zinc: number;
+zincAdjustment: any;
+zincDays: number;
 
+onAddZinc(){
+  // Set the value of zincDays based on the value of zinc
+  this.zincDays = this.calculateZincDays(this.zinc);
 
-    if (this.zinc == 10){
-        this.zincStart = 'Ideal  for most reefs'
-    }
-  //low start  7.72ml at 100 g for 0.1 ppm increase
+  // Calculate the zinc adjustment
+  this.zincAdjustment = this.calculateZincAdjustment(this.zinc, this.volumeService);
 
-    else if ( this.zinc <= 3 && this.zinc >= 0){
-      this.zincStart = `Depleted zinc Level, Correct immedietly ${this.zincAdjustment}ml per day for ${this.zincDays} days.  `
-    }
-    else if ( this.zinc <= 6 && this.zinc >= 3.1 ){
-      this.zincStart = `Low zinc level, adjust  ${this.zincAdjustment}ml per day for ${this.zincDays} days.  `
-    }
-    else if ( this.zinc <= 9 && this.zinc >= 6.1 ){
-      this.zincStart = `Reduced zinc, adjust  ${this.zincAdjustment}ml per day for ${this.zincDays} days.  `
-    }
-    else if ( this.zinc <= 9.9 && this.zinc >= 9.1 ){
-      this.zincStart = `Optimal Range for zinc, adjust ${this.zincAdjustment}ml per day for ${this.zincDays} days.`
-      }
-
-    //high start
-    else if ( this.zinc <= 12  && this.zinc >= 10.1){
-      this.zincStart = 'zinc Range Optimal '
-    }
-    else if ( this.zinc <= 25  && this.zinc >= 12.1 ){
-      this.zincStart = 'zinc slightly elevated recomendation is to allow level to settle down and watch ICP '
-    }
-    else if ( this.zinc <= 60  && this.zinc >= 25.1 ){
-      this.zincStart = 'zinc critical! elevated recomendation is preform several small water changes. 20% water change to reduce level apx 10%'
-    }
-    else {
-      this.zincStart = 'Retest parameter'
-    }
+  // Determine the zinc start message
+  switch (true) {
+    case this.zinc === this.IDEAL_ZINC_LEVEL:
+      this.zincStart = 'Ideal for most reefs';
+      break;
+    case this.zinc <= this.LOW_ZINC_LEVEL:
+      this.zincStart = `Depleted zinc Level, Correct immediately ${this.zincAdjustment}ml per day for ${this.zincDays} days.  `;
+      break;
+    case this.zinc === 0:
+      this.zincStart = `Zinc depleted, adjust ${this.zincAdjustment}ml per day for ${this.zincDays} days.  `;
+      break;
+    case this.zinc <= 4 && this.zinc >= 0.1:
+    case this.zinc <= 3 && this.zinc > this.LOW_ZINC_LEVEL:
+      this.zincStart = `Zinc depleted or very low, adjust ${this.zincAdjustment}ml per day for ${this.zincDays} days.  `;
+      break;
+    case this.zinc <= 4.9 && this.zinc >= 4.1:
+      this.zincStart = `Zinc acceptable but below target range, adjust ${this.zincAdjustment}ml per day for ${this.zincDays} days.`;
+      break;
+    case this.zinc <= 15 && this.zinc >= 5.1:
+      this.zincStart = 'Zinc acceptably above target, no concern. If it continues to increase without dosing find source';
+      break;
+    case this.zinc <= 25 && this.zinc >= 15.1:
+      this.zincStart = 'Zinc above target level, allow level to settle down and watch ICP. If it continues to increase without dosing find source';
+      break;
+    case this.zinc <= 80 && this.zinc >= 25.1:
+      this.zincStart = 'Significantly elevated Zinc! Allow level to settle down and watch ICP, strongly recommended to find source.';
+      break;
+    case this.zinc <= 200 && this.zinc >= 80.1:
+      this.zincStart = 'Critically elevated Zinc! Recommendation is perform several small water changes. 20% water change to reduce level approx 10%, find source.';
+      break;
+    default:
+      this.zincStart = 'Retest parameter';
+      break;
   }
+}
+
+calculateZincDays(zinc: number): number {
+  if (zinc >= this.MODERATE_ZINC_LEVEL) {
+    return 1;
+  } else if (zinc >= this.LOW_ZINC_LEVEL) {
+    return 2;
+  } else {
+    return 3;
+  }
+}
+
+calculateZincAdjustment(zinc: number, volumeService: VolumeService): number {
+  const zincQuantityDivisor = 5 - zinc;
+  const zincAdjustmentTotal = (0.003785 * volumeService.volume) * zincQuantityDivisor;
+
+  let zincDays: number;
+
+  if (zinc >= 3) {
+    zincDays = 1;
+  } else if (zinc >= 1) {
+    zincDays = 2;
+  } else {
+    zincDays = 3;
+  }
+
+  const zincAdjustment = zincAdjustmentTotal / zincDays;
+
+  return zincAdjustment;
+}
+
+  // zincStart: string = 'Instead of becoming fireworks, Im going to make your corals glow!'
+  // zinc: number
+  // zincAdjustment: any
+  // zincAdjustmentTotal: number
+  // zincDays: number
+  // zincQuantityDivisor: number
+
+
+
+
+  // onAddZinc(){
+
+
+
+
+
+    // This code sets the value of this.zincDays based on the value of this.zinc.
+  //   if (this.zinc >= 3)       // If this.zinc is greater than or equal to 3, this.zincDays is set to 1.
+  //   {
+  //     this.zincDays = 1;
+  //   }
+  //   else if (this.zinc >= 1)  // If this.zinc is greater than or equal to 1 but less than 3, this.zincDays is set to 2.
+  //   {
+  //     this.zincDays = 2;
+  //   }
+  //   else
+  //   {
+  //     this.zincDays = 3;      // If this.zinc is less than 1, this.zincDays is set to 3.
+  //   }
+
+
+  // // general Zinc calculation
+
+  // this.zincQuantityDivisor = (5 - this.zinc) // 410 - 4.5 = 1.5
+  // this.zincAdjustmentTotal = (0.003785  * this.volumeService.volume) * this.zincQuantityDivisor
+  // this.zincAdjustment = this.zincAdjustmentTotal / this.zincDays
+
+
+  //   if (this.zinc == 5){
+  //       this.zincStart = 'Ideal  for most reefs'
+  //   }
+  // //low start
+
+  //   else if ( this.zinc <= 3 && this.zinc >= 0){
+  //     this.zincStart = `Depleted zinc Level, Correct immedietly ${this.zincAdjustment}ml per day for ${this.zincDays} days.  `
+  //   }
+  //   else if ( this.zinc == 0){
+  //     this.zincStart = `Zinc depleted, adjust  ${this.zincAdjustment}ml per day for ${this.zincDays} days.  `
+  //   }
+  //   else if ( this.zinc <= 4 && this.zinc >= 0.1 ){
+  //     this.zincStart = `Zinc depleted or very low, adjust  ${this.zincAdjustment}ml per day for ${this.zincDays} days.  `
+  //   }
+  //   else if ( this.zinc <= 4.9 && this.zinc >= 4.1 ){
+  //     this.zincStart = `Zinc acceptable but below target range, adjust ${this.zincAdjustment}ml per day for ${this.zincDays} days.`
+  //     }
+
+  //   //high start
+  //   else if ( this.zinc <= 15  && this.zinc >= 5.1){
+  //     this.zincStart = 'Zinc acceptably above target, no concern. If it contiues to increase without dosing find source'
+  //   }
+  //   else if ( this.zinc <= 25  && this.zinc >= 15.1 ){
+  //     this.zincStart = 'Zinc above target level, allow level to settle down and watch ICP. If it contiues to increase without dosing find source'
+  //   }
+  //   else if ( this.zinc <= 80  && this.zinc >= 25.1 ){
+  //     this.zincStart = 'Significantly elevated Zinc! Allow level to settle down and watch ICP, strongly recommended to find source.'
+  //   }
+  //   else if ( this.zinc <= 200  && this.zinc >= 80.1 ){
+  //     this.zincStart = 'Critically elevated Zinc! Recomendation is preform several small water changes. 20% water change to reduce level apx 10%, find source.'
+  //   }
+  //   else {
+  //     this.zincStart = 'Retest parameter'
+  //   }
+  // }
 
 }
 
