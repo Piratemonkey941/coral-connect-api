52b13c6dbea9aa749477e11524f292ec51820544
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-04-20 12:27:23 -0500

    .

--- coral_connect/db/migrate/20211122184706_create_users.rb
+++ coral_connect/db/migrate/20211122184706_create_users.rb
@@ -16,6 +16,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   end
 end
 
-# HOW TO CREATE A USER IN CONSOLE
+# HOW TO CREATE A USER IN // console
 # User.create(email: 'example@example.com', first_name: 'John', last_name: 'Doe', phone: '1234567890', 
 # password: 'password', password_confirmation: 'password')
